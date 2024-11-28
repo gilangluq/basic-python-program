41903838fbaa9d64fb4aba4abbff1bbbaa87e106
langluq <gilangluq@gmail.com>
2024-11-29 06:44:14 +0700

    add arithmetic
    add arithmetic

---
+++ 2.addition-subtraction.py
@@ -0,0 +1,9 @@
+# Addition & Subtraction
+
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+
+sum = num1 + num2
+sub = num1 - num2
+print(f"sum: {num1} + {num2} = {sum}")
+print(f"sub: {num1} - {num2} = {sub}")
