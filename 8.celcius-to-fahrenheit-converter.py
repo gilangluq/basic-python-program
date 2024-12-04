97126071d8b8456758097b24164376f50e6e4bbe
langluq <gilangluq@gmail.com>
2024-12-05 06:20:50 +0700

    Create 8.celcius-to-fahrenheit-converter.py

---
+++ 8.celcius-to-fahrenheit-converter.py
@@ -0,0 +1,8 @@
+celcius = float(input("Enter celcius: "))
+
+# Conversion formula: (Celcius * 9/5) +32
+fahrenheit = 0.621371
+
+miles = km * conv
+
+print(f"{km} kilometers is equal to {miles} miles")
