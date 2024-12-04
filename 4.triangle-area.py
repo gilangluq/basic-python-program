38788e3a22fe306a620d215c8f93ddd0b30ded07
langluq <gilangluq@gmail.com>
2024-12-04 08:40:21 +0700

    Create 4.triangle-area.py

---
+++ 4.triangle-area.py
@@ -0,0 +1,9 @@
+# Triangle Area
+b = float(input("Triangle length: "))
+h = float(input("Triangle height: "))
+
+# Caclutae the area of the triangle
+area = 0.5 * b * h
+
+# Display the result
+print(f"The area of the triangle is: {area}")
